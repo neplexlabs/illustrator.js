b7033c500c3eef7599ea57072d1a0a65c0b94868
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-06-02 19:10:27 +0545

    chore: add docgen script

---
+++ scripts/docgen.js
@@ -0,0 +1,7 @@
+const { runGenerator } = require("@discordjs/ts-docgen");
+
+runGenerator({
+    existingOutput: "docs/typedoc.json",
+    custom: "docs/config.yml",
+    output: "docs/docs.json"
+});
