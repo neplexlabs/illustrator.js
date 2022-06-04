b1ce59851810f06bdc622f743eb87378b003284a
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-06-04 09:28:45 +0545

    chore: update pre-commit hook

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,16 @@ export * from "./layer/LayerUtils";
 export * from "./utils/IllustratorCollection";
 export * from "./layer/Layer";
 export * from "./utils/ColorUtil";
+
+// export tools individually due to docgen issue
+export * from "./toolbox/image/Image";
+export * from "./toolbox/base/ToolBox";
+export * from "./toolbox/base/BaseShapeTool";
+export * from "./toolbox/color/BackgroundColor";
+export * from "./toolbox/text/Text";
+export * from "./toolbox/filters/Filter";
+export * from "./toolbox/filters/Shadow";
+export * from "./toolbox/filters/Sharpen";
+export * from "./toolbox/filters/ConvolutionFilter";
+export * from "./toolbox/shapes/Ellipse";
+export * from "./toolbox/utilities/Transform";
