8b4f10b537777bac22fad0537b94e7f0081ec4b4
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2022-06-18 12:21:27 +0545

    fix(TextTool): flatten args

--- src/utils/makeArgs.ts
+++ src/utils/makeArgs.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
 export function makeArgs(filter?: (arg: any, idx: number, args: any[]) => boolean, ...args: any[]) {
     if (!filter || typeof filter !== "function") return args;
-    return args.filter(filter);
+    return args.filter(filter).flat(1);
 }
---
+++ test/text.mjs
@@ -0,0 +1,19 @@
+import { writeFile } from "fs/promises";
+import { ColorUtil, Illustrator } from "../dist/index.mjs";
+
+const illustrator = new Illustrator(2048, 2048);
+const layer = illustrator.backgroundLayer.unlock();
+
+// prettier-ignore
+layer.tools.get("BackgroundColorTool")
+    .setFillColor(ColorUtil.Colors.DC_DARK_BUT_NOT_BLACK)
+    .fill(0, 0, layer.width, layer.height)
+    .render();
+const text = layer.tools.get("TextTool");
+text.setFont("PRIMETIME", 72);
+text.setColor(ColorUtil.Colors.DC_WHITE);
+text.writeText("BLACKPINK", 40, 460, 700);
+text.render();
+
+const img = await illustrator.export();
+await writeFile("./text.png", img);
